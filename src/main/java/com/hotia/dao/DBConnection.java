d47d63a7a4c977382dd8e12741b297ded2844256
MOI <andyrasoanaivo43@gmail.com>
2026-06-10 18:35:44 +0300

    connection DB

---
+++ src/main/java/com/hotia/dao/DBConnection.java
@@ -0,0 +1,25 @@
+package com.hotia.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBConnection {
+
+    private static final String URL = "lien de connection de votre base de donnees";
+    private static final String USER = "nom d'utilisateur de votre base de donnees";
+    private static final String PASSWORD = "mot de passe de votre base de donnees";
+
+    private DBConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundExeption e) {
+            throw new SQLException("Driver MySQl introuvable !!", e);
+        }
+
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
