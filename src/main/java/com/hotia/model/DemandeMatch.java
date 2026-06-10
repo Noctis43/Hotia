5bb288a730cf78829ed3bd79c69f8e1d9c77e3c3
MOI <andyrasoanaivo43@gmail.com>
2026-06-10 18:43:03 +0300

    model DemandeMatch

---
+++ src/main/java/com/hotia/model/DemandeMatch.java
@@ -0,0 +1,62 @@
+package com.hotia.model;
+
+import java.time.LocalDateTime;
+
+public class DemandeMatch {
+
+    private int id;
+    private int demandeId;
+    private int cibleId;
+    private String statut;
+    private LocalDateTime dateDemande;
+
+    public DemandeMatch() {}
+
+     public DemandeMatch(int id, int demandeurId, int cibleId, String statut, LocalDateTime dateDemande) {
+        this.id = id;
+        this.demandeurId = demandeurId;
+        this.cibleId = cibleId;
+        this.statut = statut;
+        this.dateDemande = dateDemande;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDemandeurId() {
+        return demandeurId;
+    }
+
+    public void setDemandeurId(int demandeurId) {
+        this.demandeurId = demandeurId;
+    }
+
+    public int getCibleId() {
+        return cibleId;
+    }
+
+    public void setCibleId(int cibleId) {
+        this.cibleId = cibleId;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public LocalDateTime getDateDemande() {
+        return dateDemande;
+    }
+
+    public void setDateDemande(LocalDateTime dateDemande) {
+        this.dateDemande = dateDemande;
+    }
+}
